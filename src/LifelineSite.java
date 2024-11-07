e30f47bd5f33b9c99ffa74d1a9a94121a82e0c9a
Manit <manitjawa@gmail.com>
2024-11-07 15:11:08 -0500

    Updated base variable in LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -4,8 +4,13 @@ public class LifelineSite {
     private double _rate = 1.5;
 
     public double getBillableAmount(){
-        double base = _units * _rate * 0.5;
+        double base = getBaseAmount();
         double tax = base * Site.TAX_RATE * 0.2;
         return base + tax;
+    }
+
+    private double getBaseAmount() {
+        double base = _units * _rate * 0.5;
+        return base;
     }
 }
